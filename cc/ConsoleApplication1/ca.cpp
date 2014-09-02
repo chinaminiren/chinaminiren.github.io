c63f6687e314b92bf365e0f7a2f51508675c3c68
chinaminiren <renxingsong@live.cn>
2014-09-02 17:48:02 +0800

    数据结构学习
    一步一步学习

---
+++ cc/ConsoleApplication1/ca.cpp
@@ -0,0 +1,47 @@
+// CTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+
+#include <functional>
+#include <memory>
+
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <list>
+
+//设计模式
+//#include "Pattern\Prototype.h"
+//#include "Pattern\Single.h"
+//#include "Pattern\Factory.h"
+//#include "Pattern\abstractFactory.h"
+//#include "Builder.h"
+//#include "TemplateMethod.h"
+//#include "Strategy.h"
+//#include "Command.h"	
+//#include "Observer.h"
+//#include "Iterator.h"
+//#include "Mediator.h"
+//#include "visitor.h"
+//#include "State.h"
+//#include "memento.h"
+//#include "FlyWeight.h"
+//#include "Composite.h"
+
+//面向对象
+//#include "Init.h"
+
+#include "tt.h"
+
+#include <string>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	TestTT();
+	return 0;
+}
+
