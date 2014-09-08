ea3c6e7e5c4062d2b004acd8ad80083523da633a
chinaminiren <renxingsong@live.cn>
2014-09-08 21:43:20 +0800

    整理资料文档

--- cswork/stlWork/stlWork.cpp
+++ cswork/stlWork/stlWork.cpp
@@ -13,20 +13,8 @@
 
 using namespace std;
 
-class SM :std::enable_shared_from_this < SM >
-{
-public:
-	void meg()
-	{
-		cout << "meg" << endl;
-	}
-};
-
 int _tmain(int argc, _TCHAR* argv[])
 {	 
-	SM* ptr = new SM();
-	ptr->meg();
-  
 	return 0;
 }
 
