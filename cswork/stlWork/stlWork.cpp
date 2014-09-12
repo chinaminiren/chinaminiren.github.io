d958dd05128b8d07bfecc44761ef29f3ae30c288
chinaminiren <renxingsong@live.cn>
2014-09-12 13:14:14 +0800

    添加  后缀数组 题目

--- cswork/stlWork/stlWork.cpp
+++ cswork/stlWork/stlWork.cpp
@@ -9,7 +9,8 @@
 #include <functional>
 #include <string>
 #include <memory>
-
+#include <map>
+#include <set>
 
 using namespace std;
 
@@ -22,9 +23,29 @@ public:
 	}
 };
 
+class Base
+{
+public:
+	void Work(int a)
+	{
+		cout<<a<<endl;
+	}
+};
+class CBase
+{
+public:
+	void Work(Base a)
+	{
+		
+	}
+};
+
 int _tmain(int argc, _TCHAR* argv[])
 {	 
 	int** ptr =new int* [10];
+	map<int,int> mp;
+	vector<Base> vc;
+	//std::for_each(vc.begin(),vc.end(),&CBase::Work);
 	return 0;
 }
 
